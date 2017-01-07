3368b198deacc64c3c6e3a0a994a17a22da7a851
Hanju Jo <gkswn121@gmail.com>
2017-01-07 14:00:54 +0900

    04-MultiFeatures

---
+++ 04-MultiFeatures/04_linear_file_input.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+x_data = xy[0:-1]
+y_data = xy[-1]
+
+print(x_data)
+print(y_data)
+
+W = tf.Variable(tf.random_uniform([1, len(x_data)], -5.0, 5.0))
+
+# Our hypothesis
+hypothesis = tf.matmul(W, x_data)
+
+# Simplified cost function
+cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+# Minimize
+a = tf.Variable(0.1)
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+# Before starting, initialize the variables.
+init = tf.global_variables_initializer()
+
+# Lauch the graph.
+sess = tf.Session()
+sess.run(init)
+
+# Fit the line.
+for step in range(2001):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(cost), sess.run(W))
--- notes/lab02.md
+++
@@ -1,5 +0,0 @@
-# Lab 2: Linear Regression
-
-# Hypothesis and cost function
-- H(x) = Wx + b
-- cost(W, b) = ave of (H(x)-y)^2
